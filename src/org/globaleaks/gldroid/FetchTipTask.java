4b2e5b497c55bf5fc8f049a5514095538075129a
Marco Pozzato <mpodroid@gmail.com>
2013-06-20 02:07:46 +0200

    Provide feedback on tip fetching, even if UI is not implemented yet

--- src/org/globaleaks/gldroid/FetchTipTask.java
+++ src/org/globaleaks/gldroid/FetchTipTask.java
@@ -1,23 +1,14 @@
 package org.globaleaks.gldroid;
 
-import java.util.Iterator;
-
 import org.globaleaks.model.AuthSession;
-import org.globaleaks.model.Context;
 import org.globaleaks.model.Credential;
-import org.globaleaks.model.File;
 import org.globaleaks.model.Submission;
-import org.globaleaks.util.GLException;
 import org.globaleaks.util.Logger;
 
-import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.ClipData;
-import android.content.ClipboardManager;
 import android.content.DialogInterface;
-import android.content.Intent;
+import android.content.DialogInterface.OnClickListener;
 import android.os.AsyncTask;
-import android.provider.ContactsContract;
 
 public class FetchTipTask extends AsyncTask<String,Integer,AuthSession> {
 
@@ -44,38 +35,19 @@ public class FetchTipTask extends AsyncTask<String,Integer,AuthSession> {
     	}
     }
 
-	/*
 	@Override
 	protected void onPostExecute(final AuthSession session) {
-		app.setSubmission(result);
-		Logger.d("Submission receipt: " + result.getReceipt());
 		AlertDialog ad = new AlertDialog.Builder(parent)
-				.setTitle("Submission Receipt")
-				.setMessage(result.getReceipt())
-				.setPositiveButton("Add contact", new DialogInterface.OnClickListener() {
-		           public void onClick(DialogInterface dialog, int id) {		        	   
-		        	   	Logger.i("Adding receipt as a contact");
-		        	   	addReceipt(result.getReceipt());
-		           }
-		       })
-		       .setNeutralButton(android.R.string.copy, new DialogInterface.OnClickListener() {					
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						Logger.i("Copying receipt to Clipboard");
-						ClipboardManager cm = (ClipboardManager) parent.getSystemService(Activity.CLIPBOARD_SERVICE);
-						ClipData clip = ClipData.newPlainText("receipt", result.getReceipt());
-						cm.setPrimaryClip(clip);
-					}
-				})
-		       .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+				.setTitle("TIP fetched")
+				.setMessage("TODO: show tip summary for review and commenting")
+				.setPositiveButton(android.R.string.ok, new OnClickListener() {
 					
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
-						Logger.d("Receipt ignored");
+						
 					}
-				})       
-		       .create();
+				})
+				.create();
 		ad.show();
 	}
-    */
 }
